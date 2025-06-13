0e3cb5a40fb47b713760a5cd42c6dd3a3140b20f
anil-k-rajamoni18 <anil.k.rajamoni99@gmail.com>
2025-06-13 06:17:27 +0530

    update

--- LEVEL-3/sample.py
+++ LEVEL-3/sample.py
@@ -1,23 +1,22 @@
-class Person1():
-   
-    def __init__(self,surname,age): # param
-        self.c=surname
-        self.d=age
-        
-    def __init__(self): # non-param
-        print("Hey HI!!")
-             
-    def __init__(self,name): # param
-        self.a=name      
-        
-    def __init__(self,fullname,year,place): # param
-        """
-        constructor
-        """
-        self.x=fullname
-        self.y=year
-        self.z=place
+class Person:
+    '''
+        This is Person Class create object by taking name and age
+    '''
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+    def __eq__(self, other):
+        if isinstance(other, Person):
+            return self.name == other.name and self.age == other.age
+        return False
+    
+    def __repr__(self):
+        return f"Person(name={self.name!r}, age={self.age!r})"
 
-p = Person1('Alice', 45)
-print(p)
+p = Person("Alice", 30)
+print(p.__dict__)  # {'name': 'Alice', 'age': 30}
+print(p.__class__) # <class '__main__.Person'>
+print(Person.__doc__) # Prints class docstring if available
+
+print(dict.__doc__)
